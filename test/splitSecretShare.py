4cd8a6fc6e3ecf09d5dc0106aa7f857dee2ccbeb
Will Spencer <willspencer805@gmail.com>
2022-11-28 16:05:22 -0500

    print new output returned from compute_individual_shard

--- test/splitSecretShare.py
+++ test/splitSecretShare.py
@@ -1,4 +1,4 @@
-import pytest
+# import pytest
 import sys
 import json
 
@@ -10,9 +10,13 @@ if __name__ == '__main__':
     print("Retrieved identities", json.dumps(id_data, indent=2))
     recover.extract_backup_contents("backup.zip")
     print("Extracted backup file contents to current directory")
+    
     output = recover.compute_individual_shard("MOBILE_067681f4-3ae7-eda5-9ab7-4a3cc96c110a",id_data,"Mobile","metadata.json", None, None, "Thefireblocks1!")
-    print("Term for mobile shard", output)
+    print(json.dumps(output, indent=2))
+    
     output = recover.compute_individual_shard("3429798433_067681f4-3ae7-eda5-9ab7-4a3cc96c110a",id_data,"Cloud","metadata.json","priv.pem")
-    print("Term for first cloud shard", output)
+    print(json.dumps(output, indent=2))
+    
     output = recover.compute_individual_shard("923242535_067681f4-3ae7-eda5-9ab7-4a3cc96c110a",id_data,"Cloud","metadata.json", "priv.pem")
-    print("Term for second cloud shard", output)
+    print(json.dumps(output, indent=2))
+   
